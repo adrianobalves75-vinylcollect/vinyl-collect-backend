08b486e09cccbf9a71f147b6c1dad5ec88e672a5
Adriano Alves <adrianobalves75@gmail.com>
2025-10-06 15:41:17 -0300

    Initial commit: Vinyl Collect backend

---
+++ server.js
@@ -0,0 +1,11 @@
+﻿const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({ message: "Vinyl Collect API - Backend ativo!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
